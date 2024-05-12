beffcf56a396aea20996371b4e1c86c6ee247562
Shailsharma2604 <shailsharma020604@gmail.com>
2024-05-12 21:57:02 +0530

    1

--- main/draw_marker.py
+++
@@ -1,9 +0,0 @@
-import cv2 
-import numpy as np
-
-dictionary = cv2.aruco.Dictionary_get(cv2.aruco.DICT_6X6_250)
-
-markerImage = np.zeros((100, 100), dtype=np.uint8)
-markerImage = cv2.aruco.drawMarker(dictionary, 1, 200, markerImage, 1)
-
-cv2.imwrite("arucomarker.png", markerImage)
